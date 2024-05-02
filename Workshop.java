ae2f6706b62125f27bed37b18809561f5e806ca1
younj1 <129818321+younj1@users.noreply.github.com>
2024-05-02 12:43:43 -0400

    commented Workshop

--- Workshop.java
+++ Workshop.java
@@ -1,11 +1,12 @@
-public class Workshop extends Room{
-    public Workshop(int num)
+public class Workshop extends Room {
+    public Workshop(int num) // Constructor for Workshop class that takes an integer 'num'
     {
-        super(num);
-    }
-    @Override
-    public boolean isWorkshop() {
-        return true;
+        super(num); // Calling the constructor of the superclass Room with 'num'
     }
 
+    @Override
+    public boolean isWorkshop() // Overriding the isWorkshop method from the superclass Room
+    {
+        return true; // Always returning true to indicate that the room is a workshop
+    }
 }
